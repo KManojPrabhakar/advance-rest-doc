8077604da6b77330e432ea796d45d25dfac9d9e6
Manoj Prabhakar <manojprabhakar675@gmail.com>
2018-07-04 20:32:33 +0530

    first commit

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Group Chat Application",
+  "version": "0.0.1",
+  "description": "Documentation for the Group Chat App",
+  "title": "Group Chat APIs",
+  "url": "http://advance-rest.testmanoj.com",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-07-04T14:58:54.884Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
